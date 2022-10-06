9a688eea23eb34a59c7fd14763373a05eef00c0c
Noxcode99 <sgourab699@gmail.com>
2022-10-06 19:17:46 +0530

    twelvth commit

--- api/index.js
+++ api/index.js
@@ -5,25 +5,28 @@ const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
-
+const listRoute = require("./routes/lists");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-})
-.then(()=>console.log("DB connection successfull!"))
-.catch(err=> {
-  console.log(err)
-});
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.use(express.json());
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
-app.listen(8800, ()=>{
-    console.log("Backend server is running!");
+app.listen(8800, () => {
+  console.log("Backend server is running!");
 });
---
+++ api/routes/lists.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const List = require("../models/List");
+const verify = require("../verifyToken");
+
+//CREATE
+
+router.post("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    const newList = new List(req.body);
+    try {
+      const savedList = await newList.save();
+      res.status(201).json(savedList);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+
+//DELETE
+
+//GET
+
+module.exports = router;
