1621aa0d8e6c6c20add7d7992d5933b43e3b7983
Noxcode99 <sgourab699@gmail.com>
2022-10-06 14:28:32 +0530

    eleventh commit

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+
 
 dotenv.config();
 
@@ -20,6 +22,7 @@ mongoose.connect(process.env.MONGO_URL,{
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
 
 app.listen(8800, ()=>{
     console.log("Backend server is running!");
--- api/models/Movie.js
+++ api/models/Movie.js
@@ -1,19 +1,20 @@
 const mongoose = require("mongoose");
 
-const MovieSchema = new mongoose.Schema({
-    title:{ type:String, required:true, unique:true },
-    desc:{ type: String },
-    imgTitle: {type: String},
-    img:{ type: String},
-    imgSm: {type: String},
-    trailer: {type: String},
-    video: {type: String},
-    year: {type: String},
-    limit: {type: String},
-    genre: {type: String},
-    isSeries: {type,String, default: false},
-},
-{ timestamps:true }
+const MovieSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    desc: { type: String },
+    img: { type: String },
+    imgTitle: { type: String },
+    imgSm: { type: String },
+    trailer: { type: String },
+    video: { type: String },
+    year: { type: String },
+    limit: { type: Number },
+    genre: { type: String },
+    isSeries: { type: Boolean, default: false },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Movie", MovieSchema);
