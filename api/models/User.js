b7de84c7a66b5b1220c3aac61625c37d6646be89
Noxcode99 <sgourab699@gmail.com>
2022-10-11 12:10:00 +0530

    seveteen commit

--- api/models/User.js
+++ api/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+//user comment
 const UserSchema = new mongoose.Schema({
     username:{ type:String, required:true, unique:true },
     email:{ type:String, required:true, unique:true },
